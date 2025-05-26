5be6b40a2b66d1506084b34bc28efdbf1fda0869
Md Monjur Bakth Mazumder <md.monjurmbm2001@gmail.com>
2025-05-27 00:35:44 +0600

    update

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -53,3 +53,10 @@ INSERT INTO sightings (sighting_id, species_id, ranger_id, location, sighting_ti
 
 -- Problem 1
 INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
+
+
+
+
+
+-- Problem 2
+SELECT COUNT(DISTINCT species_id) AS unique_species_count FROM sightings;

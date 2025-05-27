38bf1a7e7d6cb160b9d3e534db5512d5abf48ef9
Md Monjur Bakth Mazumder <md.monjurmbm2001@gmail.com>
2025-05-28 05:42:07 +0600

    update

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -79,6 +79,46 @@ LEFT JOIN sightings ON rangers.ranger_id = sightings.ranger_id
 GROUP BY rangers.name
 
 
+-- problem 5:
+SELECT common_name      
+FROM species
+WHERE species_id NOT IN (
+  SELECT DISTINCT species_id FROM sightings
+);
 
 
 
+-- Problem 6
+SELECT species.common_name, sightings.sighting_time, rangers.name
+FROM sightings
+JOIN species ON sightings.species_id = species.species_id
+JOIN rangers ON sightings.ranger_id = rangers.ranger_id
+ORDER BY sightings.sighting_time DESC
+LIMIT 2;
+
+-- Problem 7
+UPDATE species
+SET conservation_status = 'Historic'
+WHERE discovery_date < '1800-01-01';
+
+
+
+-- Problem 8
+SELECT sighting_id,
+  CASE
+    WHEN EXTRACT(HOUR FROM sighting_time) < 12 THEN 'Morning'
+    WHEN EXTRACT(HOUR FROM sighting_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+    ELSE 'Evening'
+  END AS time_of_day
+FROM sightings;
+
+
+
+
+
+
+-- Problem 9
+DELETE FROM rangers
+WHERE ranger_id NOT IN (
+  SELECT DISTINCT ranger_id FROM sightings
+);
